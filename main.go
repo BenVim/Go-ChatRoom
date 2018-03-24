65580a44c0e7c57adcc8aa0c29bbb5eb8092c544
ben <smoilbig@126.com>
2018-03-24 22:35:11 +0800

    init..

--- main.go
+++ main.go
@@ -1,7 +1,65 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+	"time"
+)
 
-func main()  {
+var globalRoom *Room = NewRoom()
+
+type Room struct {
+	users map[string]net.Conn
+}
+
+func NewRoom() *Room {
+	return &Room{
+		users: make(map[string]net.Conn),
+	}
+}
+
+func (r *Room) Join(user string, conn net.Conn) {
+	_, ok := r.users[user]
+	if ok {
+		r.Leave(user)
+	}
+
+	r.users[user] = conn
+	fmt.Printf("%s login success!", user)
+	conn.Write([]byte(user + ":join this chat room!\n"))
+
+}
+
+func (r *Room) Leave(user string) {
+	conn, ok := r.users[user]
+
+	if !ok {
+		fmt.Printf("%v user is not exist", user)
+	}
+	conn.Close()
+	delete(r.users, user)
+	fmt.Printf("%s leave", user)
+}
+
+func (r *Room) Broadcast(who string, msg string) {
+	timeInfo := time.Now().Format("2018-03-25 23:00:00")
+	toSend := fmt.Sprintf("%v %s:%s\n", timeInfo, who, msg)
+	for user, conn := range r.users {
+		if user == who {
+			continue
+		}
+		conn.Write([]byte(toSend))
+	}
+
+}
+
+func HandleConn(conn net.Conn)  {
+	defer conn.Close()
+
+
+}
+
+func main() {
 	fmt.Println("fuck")
+
 }
